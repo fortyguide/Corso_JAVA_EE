5ab974ffe66b049037b071c215660513e9c6de01
fortyguide <stefano1994>
2022-01-26 15:31:52 +0100

    26/01/2022 Operatori

---
+++ src/operatore/OperatoriAritmetici.java
@@ -0,0 +1,15 @@
+package operatore;
+
+public class OperatoriAritmetici {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int c = a + b;
+
+        System.out.println(c);//30
+        System.out.println(a += b);//30
+
+    }
+}
