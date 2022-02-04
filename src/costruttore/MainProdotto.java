bbb61cb12d5db61b7320c5518fb4e67e9102e797
fortyguide <stefano1994>
2022-02-04 16:32:18 +0100

    04/02/2022 Polimorfismo (Inizio video)

--- src/costruttore/MainProdotto.java
+++ src/costruttore/MainProdotto.java
@@ -1,5 +1,7 @@
 package costruttore;
 
+import programmazione_a_oggetti_esempio.ereditarieta.Televisore;
+
 public class MainProdotto {
 
     public static void main(String[] args) {
@@ -7,6 +9,20 @@ public class MainProdotto {
         Prodotto pDefault = new Prodotto(); /*istanza da costruttore default*/
         Prodotto p = new Prodotto(1, "Paolo", 0);
 
-        System.out.println(p.getId() + " " + p.getNome()+ " " + p.getPrezzo());
+        System.out.println(p.getId() + " " + p.getNome()+ " " + p.getPrezzo());//1 Paolo... 1.0
+
+        Televisore t = new Televisore();
+
+        /*La classe Televisore, che estende Prodotto, eredita tutti i metodi della
+         * classe madre. Non riesce però ad accedere alle variabili di Prodotto
+         * perchè sono definite private*/
+
+        t.setId(2);
+        t.setNome("Samsung");
+        t.setPrezzo(0);
+
+        System.out.println(t.getId() + " " + t.getNome()+ " " + t.getPrezzo());//2 Samsung... 1.0
+
+
     }
 }
---
+++ src/programmazione_a_oggetti_esempio/ereditarieta/Televisore.java
@@ -0,0 +1,37 @@
+package programmazione_a_oggetti_esempio.ereditarieta;
+
+import costruttore.Prodotto;
+
+public class Televisore extends Prodotto {
+
+    private String pollici;
+    private String accessori;
+
+    public String getPollici() {
+        return pollici;
+    }
+
+    public void setPollici(String pollici) {
+        this.pollici = pollici;
+    }
+
+    public String getAccessori() {
+        return accessori;
+    }
+
+    public void setAccessori(String accessori) {
+        this.accessori = accessori;
+    }
+
+    public void cambiaCanale(){
+
+        /* corpo del metodo... */
+
+    }
+
+    public void accendi(){
+
+        /* corpo del metodo... */
+
+    }
+}
