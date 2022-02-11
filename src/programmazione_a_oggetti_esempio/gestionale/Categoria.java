51933a2a7771b5ae9bc7fddfa0313969e8a2a071
fortyguide <stefano1994>
2022-02-11 15:55:55 +0100

    11/02/2022 Polimorfismo

---
+++ src/programmazione_a_oggetti_esempio/gestionale/Categoria.java
@@ -0,0 +1,5 @@
+package programmazione_a_oggetti_esempio.gestionale;
+
+public class Categoria {
+    //01:38
+}
---
+++ src/programmazione_a_oggetti_esempio/gestionale/Cliente.java
@@ -0,0 +1,42 @@
+package programmazione_a_oggetti_esempio.gestionale;
+
+public class Cliente {
+
+    private long id;
+    private String nome;
+    private String cognome;
+    private String codiceFiscale;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getCodiceFiscale() {
+        return codiceFiscale;
+    }
+
+    public void setCodiceFiscale(String codiceFiscale) {
+        this.codiceFiscale = codiceFiscale;
+    }
+
+}
