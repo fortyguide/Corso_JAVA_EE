116c29f5f4f45727d5947c3588cb7d59971f18f6
fortyguide <stefano1994>
2022-06-03 12:00:07 +0200

    03/06/2022 Sintassi espressioni lambda

---
+++ src/espressioni_lambda/EspressioniLambdaConBreakEContinue.java
@@ -0,0 +1,22 @@
+package espressioni_lambda;
+
+public class EspressioniLambdaConBreakEContinue {
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(()->{
+            System.out.println("Sono nel thread 1");
+
+            int max = 0;
+
+            while(max < 10) {
+                System.out.println("max = " +  max);
+
+                if (max == 5) {
+                    break;
+                }
+            }
+
+        });
+    }
+}
